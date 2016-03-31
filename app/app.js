681b261b6949e99ab98eaf6fa0e89cdbb06c6236
kiran paturi <kpaturi@smarttuition.com>
2016-03-31 07:04:42 +0530

    ekart-ui checkin

---
+++ app/app.js
@@ -0,0 +1,8 @@
+//this is first line of code.
+angular.module('eKart',[
+                        "eKart.sales",
+                        "eKart.login",
+                        "eKart.register",
+                        "eKart.navigation",
+                        "eKart.cart"
+                        ]);
---
+++ app/navigation/navCtrl.js
@@ -0,0 +1,10 @@
+(
+    function(){
+        'use strict';
+        function navCtrlFn($scope){
+            
+        }
+        angular.module("eKart.navigation")
+               .controller('navCtrl',['$scope',navCtrlFn]);
+    }
+)();
